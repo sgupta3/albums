3a2d116d07f0a988e08fa73d1b568b8f026b3258
Sahil Gupta <emailsahilgupta12@gmail.com>
2016-12-11 13:43:37 -0500

    Adds Card and Card Section to simplyfy layout

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => (
-    <View>
-      <Text> { props.album.title } </Text>
-    </View>
+    <Card>
+      <CardSection>
+        <Text> { props.album.title } </Text>
+      </CardSection>
+    </Card>
   );
 
 export default AlbumDetail;
